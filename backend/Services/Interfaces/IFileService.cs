36fea7684a42591b55cf5295dd8a233c39953a69
quocdk <doanquoc281004@gmail.com>
2025-11-25 16:25:14 +0700

    redo upload file

--- backend/Services/Interfaces/IFileService.cs
+++ backend/Services/Interfaces/IFileService.cs
@@ -3,5 +3,6 @@
     public interface IFileService
     {
         Task<(bool Success, string? UrlOrError)> UploadFileAsync(IFormFile file);
+        Task<(bool Success, string? UrlOrError)> UpdateFileAsync(string fileUrl, IFormFile newFile);
     }
 }
