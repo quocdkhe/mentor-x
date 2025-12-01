fc86d0a18246a2994313d891daa79d136e026059
quocdk <doanquoc281004@gmail.com>
2025-12-01 10:21:16 +0700

    disable react strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,4 @@
 import { RouterProvider } from '@tanstack/react-router'
-import './index.css'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { router } from './routes/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -9,20 +7,19 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import './index.css'
 
 const queryClient = new QueryClient()
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <GoogleOAuthProvider clientId={googleClientId}>
-          <RouterProvider router={router} />
-        </GoogleOAuthProvider>
-        <TanStackRouterDevtools router={router} />
-        <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
-      </QueryClientProvider>
-    </Provider>
-  </StrictMode>,
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <RouterProvider router={router} />
+      </GoogleOAuthProvider>
+      <TanStackRouterDevtools router={router} />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  </Provider>
 )
