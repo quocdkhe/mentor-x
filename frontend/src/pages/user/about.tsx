ad1d9327eb88d6ef92d043ecb694ba8c9a977b10
quocdk <doanquoc281004@gmail.com>
2025-12-16 15:17:30 +0700

    new home and about

---
+++ frontend/src/pages/user/about.tsx
@@ -0,0 +1,10 @@
+import { createLazyRoute } from "@tanstack/react-router"
+
+export const Route = createLazyRoute('/user/about')({
+  component: AboutPage,
+})
+
+export default function AboutPage() {
+  return <div>Hello from About!</div>
+}
+
