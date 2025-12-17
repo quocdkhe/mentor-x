ea2b2eba3ae772393e44850a921bbc00997918d9
quocdk <doanquoc281004@gmail.com>
2025-12-17 19:46:45 +0700

    admin layout

--- frontend/src/layouts/admin.layout.tsx
+++ frontend/src/layouts/admin.layout.tsx
@@ -1,5 +1,31 @@
+import { AdminSidebar } from "@/components/dashboard/admin-sidebar.tsx"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import {ThemeToggle} from "@/components/theme-toggle.tsx";
+
 export default function AdminLayout() {
   return (
-    <h1>Hello admin</h1>
+    <SidebarProvider>
+      <AdminSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <ThemeToggle />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <p>Trang quản trị người dùng, code tại đây</p>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
