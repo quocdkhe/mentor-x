8681eac81fc2ad8226da73851c7087e477d1e80d
quocdk <doanquoc281004@gmail.com>
2026-02-23 22:04:13 +0700

    fix vercel build

--- frontend/src/components/dashboard/admin-sidebar.tsx
+++ frontend/src/components/dashboard/admin-sidebar.tsx
@@ -1,11 +1,5 @@
 import * as React from "react";
-import {
-  CheckCheckIcon,
-  CheckIcon,
-  User,
-  Users,
-  VerifiedIcon,
-} from "lucide-react";
+import { CheckCheckIcon, User, Users, VerifiedIcon } from "lucide-react";
 
 import { NavMain } from "@/components/dashboard/nav-main.tsx";
 import { NavUser } from "@/components/dashboard/nav-user.tsx";
