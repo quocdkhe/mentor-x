1a061dd362218f4e9e285d71292caa9a122d4f70
quocdk <doanquoc281004@gmail.com>
2026-02-24 21:35:14 +0700

    add migration back and is paid

--- backend/Models/Appointment.cs
+++ backend/Models/Appointment.cs
@@ -3,35 +3,26 @@ namespace backend.Models;
 public class Appointment
 {
     public Guid Id { get; set; }
-
     public Guid MentorId { get; set; }
-
     public Guid MenteeId { get; set; }
-
     /// <summary>
     /// Stored in UTC
     /// </summary>
     public DateTime StartAt { get; set; }
-
     /// <summary>
     /// Stored in UTC
     /// </summary>
     public DateTime EndAt { get; set; }
-
     /// <summary>
     /// Pending | Confirmed | Cancelled | Completed
     /// </summary>
     public AppointmentStatusEnum Status { get; set; }
-
     public string? MeetingLink { get; set; }
-    
     public string? GoogleCalendarLink { get; set; }
-
     public DateTime CreatedAt { get; set; }
-
     public DateTime UpdatedAt { get; set; }
-
     public User Mentor { get; set; }
     public User Mentee { get; set; }
+    public bool IsPaid { get; set; }
     public virtual ICollection<MentorReview> Reviews { get; set; } = new List<MentorReview>();
 }
